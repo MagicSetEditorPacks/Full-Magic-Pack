496ed9d775941601ff431b6ad2ccab1222cc0dd9
CajunAvenger <kajunkittyavenger@gmail.com>
2022-02-08 16:55:24 -0600

    NEO Update

---
+++ New folder/magic-m15-nonbasic-lands.mse-style/build.bat
@@ -0,0 +1,7 @@
+cd ..
+
+cd ..
+
+mse --create-installer magic-m15-expedition.mse-style magic-blends.mse-include magic-mana-large.mse-symbol-font magic-future-common.mse-include 
+
+ren "magic-m15-expedition.mse-installer" "Magic - Battle for Zendikar Expedition Lands.mse-installer"
