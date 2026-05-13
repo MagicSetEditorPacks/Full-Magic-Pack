a0869b31367e869f8b571f9fa6241a7740721c2b
cajun <12363371+CajunAvenger@users.noreply.github.com>
2026-05-13 15:59:52 -0500

    reinstate emblem frames

---
+++ data/magic-m15-emblem-name-cut.mse-style/build.bat
@@ -0,0 +1,7 @@
+cd ..
+
+cd ..
+
+mse --create-installer magic-m15-emblem.mse-style magic-blends.mse-include magic-mana-small.mse-symbol-font magic-future-common.mse-include magic-embossedletters.mse-symbol-font
+
+ren "magic-m15-emblem-name-cut.mse-installer" "Magic - M15 Emblem-Name-Cut.mse-installer"
