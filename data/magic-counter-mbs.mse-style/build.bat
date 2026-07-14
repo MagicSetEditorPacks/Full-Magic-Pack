d4a0223bbf124215658b3c0818aea17a3bbbdb0e
cajun <12363371+CajunAvenger@users.noreply.github.com>
2026-07-14 15:43:46 -0500

    reinstate counters

---
+++ data/magic-counter-mbs.mse-style/build.bat
@@ -0,0 +1,7 @@
+cd ..
+
+cd ..
+
+mse --create-installer magic-counter-mbs.mse-style magic.mse-game magic-blends.mse-include magic-watermarks.mse-include magic-watermarks-counter.mse-include magic-mana-small.mse-symbol-font
+
+ren "magic-counter-mbs.mse-installer" "Magic - Mirrodin Besieged Counters.mse-installer"
---
+++ data/magic-monarch-emblem-with-name.mse-style/build.bat
@@ -0,0 +1,7 @@
+cd ..
+
+cd ..
+
+mse --create-installer magic-m15-token-clear.mse-style magic.mse-game magic-watermarks.mse-include magic-mana-small.mse-symbol-font
+
+ren "magic-m15-token-clear.mse-installer" "Magic - M15 Clear Tokens.mse-installer"
